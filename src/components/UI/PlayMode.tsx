ed0924e744050bb3fe9fa75a34234ee703534d79
Achraf Chaibi <txdos123@gmail.com>
2020-12-23 23:21:43 +0100

    Changing UI

--- src/components/UI/PlayMode.tsx
+++ src/components/UI/PlayMode.tsx
@@ -15,7 +15,7 @@ export default function PlayMode(props: PlayModeCardProps) {
       <Card className="floating playmode">
         <Card.Img variant="top" src={props.image} />
         <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
+          <Card.Title className="text-title">{props.title}</Card.Title>
           <Card.Text className="description">{props.description}</Card.Text>
           <Link to={props.link}>{props.action}</Link>
         </Card.Body>
