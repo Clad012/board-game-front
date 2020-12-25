822dafcfcb5138324299d330fc38fd43b9869d2f
Achraf Chaibi <txdos123@gmail.com>
2020-12-25 21:08:32 +0100

    Changing Favicon

--- public/home/assets/js/main.js
+++ public/home/assets/js/main.js
@@ -1,124 +1,113 @@
 $(function () {
+  "use strict";
 
-    "use strict";
+  //===== Prealoder
 
-    //===== Prealoder
+  $(window).on("load", function (event) {
+    $(".preloader").delay(500).fadeOut(500);
+  });
 
-    $(window).on('load', function (event) {
-        $('.preloader').delay(500).fadeOut(500);
-    });
+  //===== Sticky
 
+  $(window).on("scroll", function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll < 20) {
+      $(".navbar-area").removeClass("sticky");
+    } else {
+      $(".navbar-area").addClass("sticky");
+    }
+  });
 
+  //===== Section Menu Active
 
-    //===== Sticky
+  var scrollLink = $(".page-scroll");
+  // Active link switching
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop();
 
-    $(window).on('scroll', function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll < 20) {
-            $(".navbar-area").removeClass("sticky");
-        } else {
-            $(".navbar-area").addClass("sticky");
+    scrollLink.each(function () {
+      if ($(this.hash).offset()) {
+        var sectionOffset = $(this.hash).offset().top - 73;
+
+        if (sectionOffset <= scrollbarLocation) {
+          $(this).parent().addClass("active");
+          $(this).parent().siblings().removeClass("active");
         }
+      }
     });
+  });
 
+  //===== close navbar-collapse when a  clicked
 
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-collapse").removeClass("show");
+  });
 
-    //===== Section Menu Active
+  $(".navbar-toggler").on("click", function () {
+    $(this).toggleClass("active");
+  });
 
-    var scrollLink = $('.page-scroll');
-    // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-toggler").removeClass("active");
+  });
 
-        scrollLink.each(function () {
+  //===== Back to top
 
-            var sectionOffset = $(this.hash).offset().top - 73;
+  // Show or hide the sticky footer button
+  $(window).on("scroll", function (event) {
+    if ($(this).scrollTop() > 600) {
+      $(".back-to-top").fadeIn(200);
+    } else {
+      $(".back-to-top").fadeOut(200);
+    }
+  });
 
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
+  //Animate the scroll to yop
+  $(".back-to-top").on("click", function (event) {
+    event.preventDefault();
 
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
 
-    //===== close navbar-collapse when a  clicked
+  //===== Svg
 
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-collapse").removeClass("show");
-    });
+  jQuery("img.svg").each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr("id");
+    var imgClass = $img.attr("class");
+    var imgURL = $img.attr("src");
 
-    $(".navbar-toggler").on('click', function () {
-        $(this).toggleClass("active");
-    });
+    jQuery.get(
+      imgURL,
+      function (data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find("svg");
 
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-toggler").removeClass('active');
-    });
-
-
-
-
-    //===== Back to top
-
-    // Show or hide the sticky footer button
-    $(window).on('scroll', function (event) {
-        if ($(this).scrollTop() > 600) {
-            $('.back-to-top').fadeIn(200)
-        } else {
-            $('.back-to-top').fadeOut(200)
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== "undefined") {
+          $svg = $svg.attr("id", imgID);
         }
-    });
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== "undefined") {
+          $svg = $svg.attr("class", imgClass + " replaced-svg");
+        }
 
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr("xmlns:a");
 
-    //Animate the scroll to yop
-    $('.back-to-top').on('click', function (event) {
-        event.preventDefault();
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+      },
+      "xml"
+    );
+  });
 
-        $('html, body').animate({
-            scrollTop: 0,
-        }, 1500);
-    });
+  //=====  WOW active
 
-
-    //===== Svg
-
-    jQuery('img.svg').each(function () {
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        jQuery.get(imgURL, function (data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
-
-            // Add replaced image's ID to the new SVG
-            if (typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if (typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass + ' replaced-svg');
-            }
-
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
-    });
-
-
-    //=====  WOW active
-
-    new WOW().init();
-
-
-
-
-
+  new WOW().init();
 });
